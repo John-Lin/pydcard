a1d5031d3e57e6de212778e578083d85ed08f132
Che-Wei Lin <linton.tw@gmail.com>
2015-07-01 17:21:49 +0800

    Add example for male female ratio

---
+++ example/simple_male_female_ratio.py
@@ -0,0 +1,22 @@
+import pydcard
+
+
+def main():
+    male = 0
+    female = 0
+
+    for page_num in range(1, 41):
+        print ('Sending request to page %d' % page_num)
+        page = pydcard.getAllPage(page_num)
+        for post_thread in range(0, len(page)):
+            if page[post_thread].get('member').get('gender') == 'M':
+                male = male + 1
+            elif page[post_thread].get('member').get('gender') == 'F':
+                female = female + 1
+            else:
+                print ('Unknown gender')
+                print (page[post_thread].get('member').get('gender'))
+    print ('Female posts: %d, Male posts: %d' % (female, male))
+
+if __name__ == '__main__':
+    main()
