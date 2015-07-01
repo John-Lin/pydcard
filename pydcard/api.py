13773d616cfb3566a5e1e04b16fc3e897734fb0b
Che-Wei Lin <linton.tw@gmail.com>
2015-07-01 17:20:13 +0800

    Add api constant

---
+++ pydcard/api.py
@@ -0,0 +1,31 @@
+POST_CONTENT_API = 'https://www.dcard.tw/api/post/all/'
+TOP_POST_API = 'https://www.dcard.tw/api/forum/all/<PageNumber>/popular'
+
+FORUM_APIS = {
+    'FORUM_ALL_API': 'https://www.dcard.tw/api/forum/all/',
+    'FORUM_MOTHER_API': 'https://www.dcard.tw/api/forum/mother/',
+    'FORUM_GRADUATE_API': 'https://www.dcard.tw/api/forum/graduate/',
+    'FORUM_FUNNY_API': 'https://www.dcard.tw/api/forum/funny/',
+    'FORUM_BG_API': 'https://www.dcard.tw/api/forum/bg/',
+    'FORUM_TRENDING_API': 'https://www.dcard.tw/api/forum/trending/',
+    'FORUM_TALK_API': 'https://www.dcard.tw/api/forum/talk/',
+    'FORUM_GIRL_API': 'https://www.dcard.tw/api/forum/girl/',
+    'FORUM_BOY_API': 'https://www.dcard.tw/api/forum/boy/',
+    'FORUM_MOOD_API': 'https://www.dcard.tw/api/forum/mood/',
+    'FORUM_MUSIC_API': 'https://www.dcard.tw/api/forum/music/',
+    'FORUM_MOVIE_API': 'https://www.dcard.tw/api/forum/movie/',
+    'FORUM_LITERATURE_API': 'https://www.dcard.tw/api/forum/literature/',
+    'FORUM_SPORT_API': 'https://www.dcard.tw/api/forum/sport/',
+    'FORUM_PET_API': 'https://www.dcard.tw/api/forum/pet/',
+    'FORUM_FOOD_API': 'https://www.dcard.tw/api/forum/food/',
+    'FORUM_JOB_API': 'https://www.dcard.tw/api/forum/job/',
+    'FORUM_STUDYABROAD_API': 'https://www.dcard.tw/api/forum/studyabroad/',
+    'FORUM_MARVEL_API': 'https://www.dcard.tw/api/forum/marvel/',
+    'FORUM_SEX_API': 'https://www.dcard.tw/api/forum/sex/',
+    'FORUM_DCARD_API': 'https://www.dcard.tw/api/forum/dcard/',
+    'FORUM_DELETE_API': 'https://www.dcard.tw/api/forum/delete/',
+    'FORUM_WHYSOSERIOUS_API': 'https://www.dcard.tw/api/forum/whysoserious/'
+}
+
+
+POST_URL = 'https://www.dcard.tw/f/all/p/'
