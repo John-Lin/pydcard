63a1a25901e6fa69290e265e83048c3195e8f6e0
Che-Wei Lin <linton.tw@gmail.com>
2015-07-01 17:30:43 +0800

    Bump to version 0.0.2

--- pydcard/__init__.py
+++ pydcard/__init__.py
@@ -1,3 +1,3 @@
 from .pydcard import getPost, getAllTopPosts, getAllNewPosts, getAllPage
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pydcard',
-      version='0.0.1',
+      version='0.0.2',
       description='This is a Dcard API wrapper',
       url='http://github.com/John-Lin/pydcard',
       author='John Lin',
